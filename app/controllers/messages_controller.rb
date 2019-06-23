7a5fa7b57603f4a8de1e8deabee1607ed02c70aa
takuto.futakawame <takuto3213@gmail.com>
2019-06-23 09:00:20 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,55 +1,55 @@
 class MessagesController < ApplicationController
-
+  bofore_action :set_message,only:[:show,:edit,:update,:destroy]
 def index
-    @messages = Message.all
+  @messages = Message.all
 end
 
 def show 
-    @message=Message.find(params[:id])
 end
 
 def new
-    @message=Message.new
+  @message=Message.new
 end
 
 def create
-    @message=Message.new(message_params)
+  @message=Message.new(message_params)
     
-    if @message.save
-        flash[:success]="Messageが正常に投稿されました"
-        redirect_to @message
-    else
-        flash.now[:danger]="Messageが投稿されませんでした"
-        render :new
-    end
+  if @message.save
+    flash[:success]="Messageが正常に投稿されました"
+    redirect_to @message
+  else
+    flash.now[:danger]="Messageが投稿されませんでした"
+    render :new
+  end
 end
 
 def edit 
-    @message=Message.find(params[:id])
 end
 
 def update
-    @message=Message.find(params[:id])
     
-    if @message.update(message_params)
-        flash[:success]="Messageは正常に更新されました"
-        redirect_to @message
-    else 
-        flash[:danger]="Messageは更新されませんでした"
-        render :edit
-    end
+  if @message.update(message_params)
+    flash[:success]="Messageは正常に更新されました"
+    redirect_to @message
+  else 
+    flash[:danger]="Messageは更新されませんでした"
+    render :edit
+  end
 end
 
 def destroy
-    
-    @message=Message.find(params[:id])
-    @message.destroy
-    flash[:success]="Messageは正常に削除されました"
-    redirect_to messages_url
+  @message.destroy
+  flash[:success]="Messageは正常に削除されました"
+  redirect_to messages_url
 end
 
 private
+
+def set_message
+  @message=Message.find(params[:id]) 
+end
+
 def message_params
-    params.require(:message).permit(:content)
+  params.require(:message).permit(:content)
 end
 end
